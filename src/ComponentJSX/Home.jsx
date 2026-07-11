8ee069a41fe37301a493aeba0ce6d41ad3154fc5
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-07-11 20:48:07 +0530

    jdsk

--- src/ComponentJSX/Home.jsx
+++ src/ComponentJSX/Home.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
+
 import Hero from './Hero'
 import About from './About'
+import Navbar from './Navbar'
 import ZenitsuSword from './ZenitsuSword'
 import TeamInfo from './TeamInfo'
+import ThunderBreathing from './ThunderBreathing'
+import Cursor from './Cursor'
 
 const Home = () => {
     return (
-        <>
-            <section id='HomeHeroSection'>
-                <Hero />
-            </section>
-
-            <section id='AboutFirstSection'>
-                <About />
-            </section>
-
-            <section id='Zenitsu Sword Section'>
-                <ZenitsuSword />
-            </section>
-
-            <section id='TeamInfoSection'>
-                <TeamInfo />
-            </section>
-        </>
+        <div className="ZenitsuLandingApp">
+            <Cursor />
+            <Navbar />
+            <div id="HomeHeroSection"><Hero /></div>
+            <div id="AboutFirstSection"><About /></div>
+            <div><ZenitsuSword /></div>
+            <div id="TeamInfoSection"><TeamInfo /></div>
+            <div><ThunderBreathing /></div>
+        </div>
     )
 }
 
